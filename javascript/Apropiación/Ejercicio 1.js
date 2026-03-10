ddc96b2858216d3c72132724899a25899b991732
Oscar <mao090997@gmail.com>
2026-03-10 10:13:02 -0500

    Trabajo de oscar: Apro=1, Transfe=1

---
+++ javascript/Apropiación/Ejercicio 1.js
@@ -0,0 +1,10 @@
+// Expresiones
+
+console.log(10 > 5);     // true
+console.log(8 <= 8);     // true
+console.log(7 != "7");   // false
+console.log(4 === 4);    // true
+console.log(6 == "6");   // true
+
+//Al ejecutarlos en web, le aparecerá expresiones booleanas,
+//Siendo el resultado de cada expresión
---
+++ javascript/Transferencia/Ejercicio 1.js
@@ -0,0 +1,13 @@
+//Acceso a un sistema
+
+let usuario = "admin";
+let contraseña = "1234";
+
+if (usuario === "admin" && contraseña === "1234") {
+    console.log("Acceso permitido");
+} else {
+    console.log("Acceso denegado");
+}
+
+//Se usa el operador lógico AND (&&), porque las dos
+//condiciones deben cumplirse al mismo tiempo
